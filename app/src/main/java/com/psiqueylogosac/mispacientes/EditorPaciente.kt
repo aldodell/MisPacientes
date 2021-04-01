46ced59fa35000f2ffbc6061569cf5d53af68351
aldodell <aldodell@gmail.com>
2021-04-01 09:49:25 -0400

    Desarrollando

--- app/src/main/java/com/psiqueylogosac/mispacientes/EditorPaciente.kt
+++ app/src/main/java/com/psiqueylogosac/mispacientes/EditorPaciente.kt
@@ -2,10 +2,82 @@ package com.psiqueylogosac.mispacientes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class EditorPaciente : AppCompatActivity() {
+
+    lateinit var nombresEt: EditText
+    lateinit var apellidosEt: EditText
+    lateinit var cedulaEt: EditText
+    lateinit var correoElectronicoEt: EditText
+    lateinit var celularEt: EditText
+    lateinit var fechaNacimientoEt: EditText
+    lateinit var anamnesisEt: EditText
+    lateinit var notasEt: EditText
+    lateinit var sexo: SwitchMaterial
+    lateinit var salvar: Button
+    lateinit var descartar: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor_paciente)
+        nombresEt = findViewById(R.id.nombresEt)
+        apellidosEt = findViewById(R.id.apellidosEt)
+        cedulaEt = findViewById(R.id.cedulaEt)
+        correoElectronicoEt = findViewById(R.id.correoElectronicoEt)
+        celularEt = findViewById(R.id.celularEt)
+        fechaNacimientoEt = findViewById(R.id.fechaNacimientoEt)
+        anamnesisEt = findViewById(R.id.anamnesisEt)
+        notasEt = findViewById(R.id.notasEt)
+        sexo = findViewById(R.id.sexoSw)
+        salvar = findViewById(R.id.salvarBt)
+        descartar = findViewById(R.id.descartarBt)
+
+
+        sexo.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                sexo.text = getString(R.string.hombre)
+            } else  {
+                sexo.text = getString(R.string.mujer)
+            }
+
+        }
+
+        salvar.setOnClickListener {
+
+            var s = ""
+            if (sexo.isChecked) {
+                s = "H"
+            } else {
+                s = "M"
+            }
+            var paciente = Paciente(
+                0,
+                apellidosEt.text.toString(),
+                nombresEt.text.toString(),
+                s,
+                cedulaEt.text.toString(),
+                fechaNacimientoEt.text.toString(),
+                anamnesisEt.text.toString(),
+                notasEt.text.toString(),
+                celularEt.text.toString(),
+                correoElectronicoEt.text.toString()
+
+            )
+
+            Thread {
+                baseDatos.pacienteDao().insertar(paciente)
+                finish()
+            }.join()
+
+
+        }
+
+        descartar.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/psiqueylogosac/mispacientes/MainActivity.kt
+++ app/src/main/java/com/psiqueylogosac/mispacientes/MainActivity.kt
@@ -7,25 +7,28 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-lateinit var pacientes_rv : RecyclerView
+
 lateinit var baseDatos : AppBaseDatos
 var pacientes = ArrayList<Paciente>()
-lateinit var fab : FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var pacientes_rv : RecyclerView
+    lateinit var fab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         baseDatos = Room.databaseBuilder(applicationContext,AppBaseDatos::class.java,"pacientes").build()
+
+
         fab = findViewById(R.id.fab)
 
 
 
         //Asignamos la repsuesta al boton agregar paciente
         fab.setOnClickListener {
-
             var intent = Intent(it.context, EditorPaciente::class.java)
             startActivity(intent)
         }
