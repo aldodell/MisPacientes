04c40314e33977eb87a21cbda9ea04c55e9a82f6
aldodell <aldodell@gmail.com>
2021-04-04 13:07:09 -0400

    desarrollo 6

---
+++ app/src/main/java/com/psiqueylogosac/mispacientes/ListaCitas.kt
@@ -0,0 +1,11 @@
+package com.psiqueylogosac.mispacientes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaCitas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_citas)
+    }
+}
