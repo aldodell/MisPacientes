535653d1544f3d1889ab291c3185cfeadce454c6
Aldo Dell Uomini <aldodell@gmail.com>
2021-05-18 11:24:35 -0400

    En la mackbook air

--- app/src/main/java/com/psiqueylogosac/mispacientes/AjustesActivity.kt
+++ app/src/main/java/com/psiqueylogosac/mispacientes/AjustesActivity.kt
@@ -3,10 +3,12 @@ package com.psiqueylogosac.mispacientes
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.doOnTextChanged
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -18,9 +20,10 @@ class AjustesActivity : AppCompatActivity() {
     lateinit var contrasenaConfirmacionEt: EditText
     lateinit var crearUsuario: ImageButton
     lateinit var identificarUsuario: ImageButton
+    lateinit var barraProgreso : ProgressBar
+    lateinit var crearUsuarioEtiqueta : TextView
+    lateinit var identificarUsuarioEtiqueta : TextView
 
-    // lateinit var respaldarDatos: Button
-    //  lateinit var recuperarDatos: Button
     val autorizador = FirebaseAuth.getInstance()
     lateinit var prefs: SharedPreferences
 
@@ -33,47 +36,97 @@ class AjustesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ajustes)
 
-
+        // Identificamos los componentes
         correoElectronicoEt = findViewById(R.id.ajustesCorreoElectronico)
         contrasenaEt = findViewById(R.id.ajustesContrasena)
         contrasenaConfirmacionEt = findViewById(R.id.ajustesContrasenaConfirmacion)
         crearUsuario = findViewById(R.id.ajustesCrearUsuario)
         identificarUsuario = findViewById(R.id.ajustesIdentificarUsuario)
-
-        crearUsuario.isEnabled = false
-        identificarUsuario.isEnabled = false
-
-        // respaldarDatos = findViewById(R.id.ajustesRespaldarDatos)
-        // recuperarDatos = findViewById(R.id.ajustesRecuperarDatos)
-
-        // respaldarDatos.isEnabled = false
-        // recuperarDatos.isEnabled = false
+        barraProgreso = findViewById(R.id.ajustesProgreso)
+        crearUsuarioEtiqueta = findViewById(R.id.ajustesCrearUsuarioEtiqueta)
+        identificarUsuarioEtiqueta = findViewById(R.id.ajustesIdentificarUsaurioEtiqueta)
 
 
+
+
+        //Inhabilitamos botones
+        configurarInterfaz(false,false, false)
+
+
+        //Obtenemos acceso a las preferencias
         prefs = this.getSharedPreferences("ajustes", MODE_PRIVATE)
 
+        //Si el correo no es null entonces procesamos la autorizacion
         if (prefs.getString("correoElectronico", null) != null) {
             correoElectronicoEt.setText(prefs.getString("correoElectronico", ""))
             contrasenaEt.setText(prefs.getString("contrasena", ""))
             contrasena = contrasenaEt.text.toString().trim()
             correoElectronico = correoElectronicoEt.text.toString().trim()
-            identificarUsuario.isEnabled = true
+            configurarInterfaz(false,true)
+        } else {
+            configurarInterfaz(true,false)
         }
 
 
-        correoElectronicoEt.setOnFocusChangeListener { view, hasFocus ->
-            if (!hasFocus) {
-                correoElectronico = correoElectronicoEt.text.toString().trim()
-
-
+        correoElectronicoEt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO("Not yet implemented")
             }
-        }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(correoElectronico != correoElectronicoEt.text.toString().trim())
+                {
+                    configurarInterfaz(true,true,false)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                correoElectronico = correoElectronicoEt.text.toString().trim()
+            }
+
+        })
+
+
+
+        contrasenaEt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                contrasena = contrasenaEt.text.toString().trim()
+            }
+
+        })
+
+
+        contrasenaConfirmacionEt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                contrasenaConfirmacion = contrasenaConfirmacionEt.text.toString().trim()
+            }
+
+        })
+
+
+
+
 
 
         contrasenaEt.setOnFocusChangeListener { view, hasFocus ->
             if (!hasFocus) {
                 contrasena = contrasenaEt.text.toString().trim()
-                identificarUsuario.isEnabled = true
                 if (contrasena.length < 6) {
                     AlertDialog.Builder(view.context)
                         .setMessage(R.string.contrasena_invalida)
@@ -81,24 +134,19 @@ class AjustesActivity : AppCompatActivity() {
                         .show()
                 }
             }
-
-
         }
 
 
         contrasenaConfirmacionEt.setOnFocusChangeListener { view, hasFocus ->
 
             if (!hasFocus) {
-                crearUsuario.isEnabled = true
-                contrasenaConfirmacion = contrasenaConfirmacionEt.text.toString().trim()
                 if (contrasenaConfirmacion.toString().length > 1) {
                     if (contrasena != contrasenaConfirmacion) {
                         AlertDialog.Builder(view.context)
                             .setMessage(R.string.contrasenas_no_coinciden)
                             .setNeutralButton(R.string.ok) { d, _ -> d.dismiss() }
                             .show()
-                    } else {
-                        crearUsuario.isEnabled = true
+
                     }
                 }
             }
@@ -106,39 +154,52 @@ class AjustesActivity : AppCompatActivity() {
 
         //Creamos un usuario nuevo. Hay que verificar si ya existe en la base de datos
         crearUsuario.setOnClickListener {
-            autorizador.createUserWithEmailAndPassword(
-                correoElectronico,
-                contrasena
-            ).addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
+            configurarInterfaz(false,false,true)
 
-                    guardarUsuarioLocalmente()
+            if(contrasena != contrasenaConfirmacion) {
+                AlertDialog.Builder(it.context)
+                    .setMessage(R.string.contrasenas_no_coinciden)
+                    .setNeutralButton(R.string.ok) { _,_ -> configurarInterfaz(true,true,false) }
+                    .show()
 
-                    AlertDialog.Builder(it.context)
-                        .setMessage(R.string.usuario_creado)
-                        .setNeutralButton(R.string.ok) { d, _ -> d.dismiss() }
-                        .show()
-                } else {
-                    AlertDialog.Builder(it.context)
-                        .setMessage(getString(R.string.error_creando_usuario) + " " + task.exception!!.message!!)
-                        .setNeutralButton(R.string.ok) { d, _ -> d.dismiss() }
-                        .show()
+            } else {
+
+                autorizador.createUserWithEmailAndPassword(
+                    correoElectronico,
+                    contrasena
+                ).addOnCompleteListener(this) { task ->
+                    configurarInterfaz(false, true, false)
+                    if (task.isSuccessful) {
+                        guardarUsuarioLocalmente()
+                        AlertDialog.Builder(it.context)
+                            .setMessage(R.string.usuario_creado)
+                            .setNeutralButton(R.string.ok) { _, _ ->
+                                finish()
+                            }
+                            .show()
+                        configurarInterfaz(false, true)
+                    } else {
+                        AlertDialog.Builder(it.context)
+                            .setMessage(getString(R.string.error_creando_usuario) + " " + task.exception!!.message!!)
+                            .setNeutralButton(R.string.ok) { d, _ -> d.dismiss() }
+                            .show()
+                    }
                 }
             }
-
         }
 
         //Identificamos el usuario (log in)
         identificarUsuario.setOnClickListener {
+            configurarInterfaz(false,false,true)
             autorizador.signInWithEmailAndPassword(correoElectronico, contrasena)
                 .addOnCompleteListener(this)
                 { task ->
+                    configurarInterfaz(false,true,false)
                     if (task.isSuccessful) {
                         guardarUsuarioLocalmente()
-
                         AlertDialog.Builder(it.context)
                             .setMessage(R.string.acceso_positivo)
-                            .setNeutralButton(R.string.ok) { d, _ -> d.dismiss() }
+                            .setNeutralButton(R.string.ok) { _,_ -> finish()  }
                             .show()
                     } else {
                         AlertDialog.Builder(it.context)
@@ -160,8 +221,47 @@ class AjustesActivity : AppCompatActivity() {
             putString("contrasena", contrasena)
             putString("usuarioId", usuarioId)
         }.apply()
+    }
+
+    fun configurarInterfaz(crear: Boolean?, identificar : Boolean?, procesando: Boolean?=null){
+
+        crearUsuario.isEnabled = false
+        identificarUsuario.isEnabled = false
+        crearUsuarioEtiqueta.isEnabled = false
+        identificarUsuarioEtiqueta.isEnabled = false
+
+
+
+        crear?.let {
+            correoElectronicoEt.isEnabled = it
+            contrasenaEt.isEnabled = it
+            contrasenaConfirmacionEt.isEnabled = it
+            crearUsuarioEtiqueta.isEnabled = it
+            crearUsuario.isEnabled = it
+        }
+
+        identificar?.let {
+            correoElectronicoEt.isEnabled = it
+            contrasenaEt.isEnabled = it
+            contrasenaConfirmacionEt.isEnabled = it
+            identificarUsuarioEtiqueta.isEnabled = it
+            identificarUsuario.isEnabled = it
+        }
+
+
+        procesando?.let {
+            if(it) {
+                barraProgreso.visibility = View.VISIBLE
+            } else {
+                barraProgreso.visibility = View.INVISIBLE
+            }
+        }
 
 
     }
+
+
+
+
 
 }
